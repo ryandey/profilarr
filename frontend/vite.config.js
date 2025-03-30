f2b7e35005e474b0e8927f2957c2c15b99e17c5b
Ryan Dey <ryandeyp@gmail.com>
2025-03-30 18:49:41 -0500

    Add additional frontend prep

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+            '@/*': path.resolve(__dirname, './src/*'),
             '@components': path.resolve(__dirname, './src/components'),
             '@hooks': path.resolve(__dirname, './src/hooks'),
             '@constants': path.resolve(__dirname, './src/constants'),
