139ce0164c6654408b1ebcb14648d26fb5398ac2
Ryan Dey <ryandeyp@gmail.com>
2025-03-31 18:53:04 -0500

    Fix Container primtive's mobile responsiveness

--- frontend/src/components/ui/Container.jsx
+++ frontend/src/components/ui/Container.jsx
@@ -1,6 +1,12 @@
-const Container = ({children}) => {
+import {cn} from '@/lib/utils';
+
+const Container = ({children, className}) => {
     return (
-        <div className='w-full flex flex-col gap-4 p-8 bg-linear-to-br from-white-50 to-gray-50 border border-gray-300 rounded-lg shadow-lg overflow-hidden dark:from-gray-800 dark:to-gray-900 dark:border-gray-700 dark:shadow-xl'>
+        <div
+            className={cn(
+                'w-full flex flex-col gap-4 p-6 md:p-8 bg-linear-to-br from-white-50 to-gray-50 border border-gray-300 rounded-lg shadow-lg overflow-hidden dark:from-gray-800 dark:to-gray-900 dark:border-gray-700 dark:shadow-xl',
+                className
+            )}>
             {children}
         </div>
     );
